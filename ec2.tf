e1552ad08730896db2e716b3aa77b0574b07bd5b
Uma Mahesh <adabala.um@gmail.com>
2025-05-28 13:18:22 +0530

    EC2 Created

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "my_ec2" {
+  ami                    = var.ami_id
+  instance_type          = var.ins_type
+  key_name               = "Mac_Login"
+  user_data              = file("apache-install.sh")
+  vpc_security_group_ids = [aws_security_group.http.id, aws_security_group.ssh.id]
+  tags = {
+    "Name" = "my_ec2"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+  description = "AMI-ID"
+  type        = string
+  default     = "ami-0af9569868786b23a"
+}
+
+variable "ins_type" {
+  description = "Instance-type"
+  type        = string
+  default     = "t2.micro"
+}
